1fe7302b4ff4e6d5e91455fe37e5190198ad7b8a
MacBook Pro <macbookpro@MacBook-Pro-3.local>
2019-03-26 00:06:39 +0100

    Done

---
+++ script.js
@@ -0,0 +1,36 @@
+function greet() {
+    window.alert("Hello everyone");
+}
+
+function init() {
+    
+    document.getElementById("first").src = "flower2.png"; 
+    
+    // document.getElementById("container").getElementsByTagName("img")[1].src = "flower2.png";
+    // document.getElementById("container").getElementsByTagName("img")[3].src = "flower2.png";
+    
+    let images = document.getElementById("container").getElementsByTagName("img");
+    images[1].src="flower2.png";
+    images[3].src="flower2.png";
+    
+    document.getElementById("task3").innerHTML = "<div><p>Your are great!</p></div>";
+    
+    let image = document.createElement("img");
+    image.src="flower2.png";
+    document.getElementById("new_element").appendChild(image);
+
+    let spans = document.getElementsByClassName("yo");
+    let colors = ["red", "orange","yellow","green","blue","purple","pink"];
+    for (var i = spans.length - 1; i >= 0; i--) {
+        spans[i].style.color = colors[i];
+    }
+
+    var changeSrc = function(event) {
+        if (event.target.src) {
+          event.target.src = "flower2.png";
+        }
+      };
+    document.getElementById("event").addEventListener("mouseover", changeSrc);
+    let filename = event.target.src.replace(/^.*[\\\/]/, '');
+}
+
